3c2506b06fdd589f1b627eacb3d44a77d693f197
James Riall <jriall@google.com>
2018-03-11 22:11:27 +0000

    Completed to end of lesson 79

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  displayRecipes = true;
+  displayShoppingList = false;
+
+  selectView(view) {
+    if (view === 'Recipes') {
+      this.displayRecipes = true;
+      this.displayShoppingList = false;
+    }
+    if (view === 'Shopping List') {
+      this.displayRecipes = false;
+      this.displayShoppingList = true;
+    }
+  }
 }
--- recipe-app/src/app/header/header.component.ts
+++ recipe-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() recipeEmitter = new EventEmitter<{  }>();
+  @Output() shoppingListEmitter = new EventEmitter<{ }>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectRecipes() {
+    this.recipeEmitter.emit('Recipes');
+  }
+
+  selectShoppingList() {
+    this.shoppingListEmitter.emit('Shopping List');
   }
 
 }
