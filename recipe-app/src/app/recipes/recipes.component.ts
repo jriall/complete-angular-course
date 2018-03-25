1f14efe04e3d822f11f52460fe34ef6233d4124a
James Riall <james.riall@phntms.com>
2018-03-25 15:20:20 +0100

    Completed http section

--- recipe-app/src/app/recipes/recipes.component.ts
+++ recipe-app/src/app/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RecipeService } from './recipe.service';
-import { Recipe } from './recipe.model';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -9,16 +9,11 @@ import { Recipe } from './recipe.model';
   providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
+
   }
 
 }
